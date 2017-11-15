d41b6c902d30fa6e17177605987bc6ef23f54222
howie6879 <xiaozizayang@gmail.com>
2017-11-15 11:23:17 +0800

    11_15

---
+++ sanic_0.1.2/tests/performance/golang/golang.http.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":" + os.Args[1], nil)
+}
